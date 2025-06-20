46252d17d46b91b9792b2324903dfa7db5fc5e4c
Talita L. Lima <ludmilalima@outlook.com>
2025-06-20 18:34:44 +0000

    add if-elif-else exercise for number classification

---
+++ exercises/if-elif-else.py
@@ -0,0 +1,15 @@
+a = int(input("Digite um número entre 0 e 100:"))
+
+
+if (a < 25):
+    print("Maior que 25!")
+elif(a > 35):
+    print("Maior que 35!")
+elif( a < 30):
+    print("Menor que 25!")
+else:
+    print("pegadinha")
+if (a > 50):
+    print("Maior que 50!")
+else:
+    print("Não sei dizer!")
